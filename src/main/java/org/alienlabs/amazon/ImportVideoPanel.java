e9a3debc4d13100e8ee2254f9ee8629f3aee7ed0
AlienQueen @ AlienLabs <goupilpierre@gmail.com>
2015-08-26 17:02:45 +0200

    Introduced Sphinx 4

--- src/main/java/org/alienlabs/amazon/ImportVideoPanel.java
+++ src/main/java/org/alienlabs/amazon/ImportVideoPanel.java
@@ -24,112 +24,102 @@ import org.apache.wicket.util.lang.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
-import com.xuggle.mediatool.IMediaReader;
-import com.xuggle.mediatool.IMediaWriter;
-import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.mediatool.IMediaViewer;
-import com.xuggle.xuggler.ICodec;
 
-@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = { "SE_INNER_CLASS",
-"SIC_INNER_SHOULD_BE_STATIC_ANON" }, justification = "In Wicket, serializable inner classes are common. And as the parent Page is serialized as well, this is no concern. This is no bad practice in Wicket")
-public class ImportVideoPanel extends Panel
-{
-	private static final Logger LOGGER = LoggerFactory.getLogger(ImportVideoPanel.class);
-	private static final long serialVersionUID = 1L;
-	final FileUploadField file;
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = {"SE_INNER_CLASS",
+        "SIC_INNER_SHOULD_BE_STATIC_ANON"}, justification = "In Wicket, serializable inner classes are common. And as the parent Page is serialized as well, this is no concern. This is no bad practice in Wicket")
+public class ImportVideoPanel extends Panel {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImportVideoPanel.class);
+    private static final long serialVersionUID = 1L;
+    final FileUploadField file;
 
-	public ImportVideoPanel(final String id)
-	{
-		super(id);
-		Injector.get().inject(this);
+    public ImportVideoPanel(final String id) {
+        super(id);
+        Injector.get().inject(this);
 
-		final Form<Void> form = new Form<>("form");
-		this.file = new FileUploadField("videoFile");
-		form.setMarkupId("inputForm").setOutputMarkupId(true);
-		form.setMaxSize(Bytes.kilobytes(5));
-		form.setMultiPart(true);
-		form.add(this.file);
-		this.add(form);
+        final Form<Void> form = new Form<>("form");
+        this.file = new FileUploadField("videoFile");
+        form.setMarkupId("inputForm").setOutputMarkupId(true);
+        form.setMaxSize(Bytes.kilobytes(5));
+        form.setMultiPart(true);
+        form.add(this.file);
+        this.add(form);
 
-		final Button upload = new Button("upload")
-		{
-			@Override
-			public void onSubmit()
-			{
-				ImportVideoPanel.LOGGER.info("trying to upload something");
+        final Button upload = new Button("upload") {
+            @Override
+            public void onSubmit() {
+                ImportVideoPanel.LOGGER.info("trying to upload something");
 
-				//final FileUpload fupload = ImportDeckDialog.this.file.getFileUpload();
-				//if (fupload == null)
-				//{
-					// No file was provided
-				//	ImportDeckDialog.LOGGER.info("Please provide a valid file");
-				//	return;
-				//}
-				//else if (fupload.getSize() == 0)
-				//{
-				//	ImportDeckDialog.LOGGER.info("Please provide a non-empty file");
-				//	return;
-				//}
-				//else if ((fupload.getClientFileName() == null)
-				//		|| ("".equals(fupload.getClientFileName().trim()))
-				//		|| (fupload.getClientFileName().endsWith(".txt")))
-				//{
-				//	ImportDeckDialog.LOGGER.info("Please provide a valid file");
-				//	return;
-				//}
+                //final FileUpload fupload = ImportDeckDialog.this.file.getFileUpload();
+                //if (fupload == null)
+                //{
+                // No file was provided
+                //	ImportDeckDialog.LOGGER.info("Please provide a valid file");
+                //	return;
+                //}
+                //else if (fupload.getSize() == 0)
+                //{
+                //	ImportDeckDialog.LOGGER.info("Please provide a non-empty file");
+                //	return;
+                //}
+                //else if ((fupload.getClientFileName() == null)
+                //		|| ("".equals(fupload.getClientFileName().trim()))
+                //		|| (fupload.getClientFileName().endsWith(".txt")))
+                //{
+                //	ImportDeckDialog.LOGGER.info("Please provide a valid file");
+                //	return;
+                //}
 
-				//ImportDeckDialog.LOGGER.info("uploading file: "
-				//		+ ImportDeckDialog.this.file.getFileUpload().getClientFileName());
+                //ImportDeckDialog.LOGGER.info("uploading file: "
+                //		+ ImportDeckDialog.this.file.getFileUpload().getClientFileName());
 
-				//try
-				//{
-				//	new String(fupload.getBytes(), "UTF-8");
-				ImportVideoPanel.convert("/home/nostromo/test.mkv", "/home/nostromo/test.wav");
-				//}
-				//catch (final UnsupportedEncodingException e)
-				//{
-				//	ImportDeckDialog.LOGGER.info("Please provide a file encoded with UTF-8 charset");
-				//	return;
-				//}
+                //try
+                //{
+                //	new String(fupload.getBytes(), "UTF-8");
+                ImportVideoPanel.convert("/home/nostromo/test.mkv", "/home/nostromo/test.wav");
+                //}
+                //catch (final UnsupportedEncodingException e)
+                //{
+                //	ImportDeckDialog.LOGGER.info("Please provide a file encoded with UTF-8 charset");
+                //	return;
+                //}
 
-				//ImportDeckDialog.LOGGER.info("successfully added deck: "
-				//		+ fupload.getClientFileName());
-				ImportVideoPanel.LOGGER.info("Your file has been successfully uploaded");
-			}
-		};
-		
-		form.add(upload);
-	}
-	
-	public static void convert(String from, final String to) { 
-		//Runtime.getRuntime().exec(
-		//	"ffmpeg -i " + from + "-acodec pcm_s16le -ac 2 " + to);
-		try {
-		ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", from, "-acodec", "pcm_s16le", "-ac", "2", to);
-		System.out.println("Run ffmpeg command");
-		Process process = pb.start();
-		int errCode = process.waitFor();
-		System.out.println("ffmpeg command executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
-		System.out.println("ffpmeg Output:\n" + output(process.getInputStream()));   
-		}
-		catch (Exception e) {
-			ImportVideoPanel.LOGGER.error("error fonverti g video to wav", e);
-		}
-	}
-	
-	private static String output(InputStream inputStream) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = null;
-		
-		try {
-			br = new BufferedReader(new InputStreamReader(inputStream));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				sb.append(line + System.getProperty("line.separator"));
-			}
-		} finally {
-			br.close();
-		}
-		return sb.toString();
-	}
+                //ImportDeckDialog.LOGGER.info("successfully added deck: "
+                //		+ fupload.getClientFileName());
+                ImportVideoPanel.LOGGER.info("Your file has been successfully uploaded");
+            }
+        };
+
+        form.add(upload);
+    }
+
+    public static void convert(String from, final String to) {
+        //Runtime.getRuntime().exec(
+        //	"ffmpeg -i " + from + "-acodec pcm_s16le -ac 2 " + to);
+        try {
+            ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", from, "-acodec", "pcm_s16le", "-ac", "2", to);
+            System.out.println("Run ffmpeg command");
+            Process process = pb.start();
+            int errCode = process.waitFor();
+            System.out.println("ffmpeg command executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+            System.out.println("ffpmeg Output:\n" + output(process.getInputStream()));
+        } catch (Exception e) {
+            ImportVideoPanel.LOGGER.error("error fonverti g video to wav", e);
+        }
+    }
+
+    private static String output(InputStream inputStream) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + System.getProperty("line.separator"));
+            }
+        } finally {
+            br.close();
+        }
+        return sb.toString();
+    }
 }
---
+++ src/main/java/org/alienlabs/amazon/TranscriberDemo.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2013 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ */
+package org.alienlabs.amazon;
+
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.decoder.adaptation.Stats;
+import edu.cmu.sphinx.decoder.adaptation.Transform;
+import edu.cmu.sphinx.result.WordResult;
+
+/**
+ * A simple example that shows how to transcribe a continuous audio file that
+ * has multiple utterances in it.
+ */
+public class TranscriberDemo {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Loading models...");
+
+        Configuration configuration = new Configuration();
+
+        // Load model from the jar
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+
+        // You can also load model from folder
+        // configuration.setAcousticModelPath("file:en-us");
+
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = TranscriberDemo.class.getResourceAsStream("test2.wav");
+        //.getResourceAsStream("/edu/cmu/sphinx/demo/aligner/10001-90210-01803.wav");
+        //stream.skip(44);
+
+        // Simple recognition with generic model
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        StringBuilder output = new StringBuilder();
+
+        while ((result = recognizer.getResult()) != null) {
+
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+
+            System.out.println("List of recognized words and their times:");
+            for (WordResult r : result.getWords()) {
+                output.append(r);
+                System.out.println(r);
+            }
+
+            System.out.println("Best 3 hypothesis:");
+            for (String s : result.getNbest(3))
+                System.out.println(s);
+
+        }
+        recognizer.stopRecognition();
+        System.out.println("Final result: " + output.toString());
+    }
+}
